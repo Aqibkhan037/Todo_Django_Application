9b721b515d3ad67981c5928983a931e9579b1acf
Aqib Khan <aqibkhan1582000@gmail.com>
2025-09-15 22:08:11 +0500

    please commit

---
+++ Backend/todos/views.py
@@ -0,0 +1,11 @@
+
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by('-created')
+    serializer_class = TodoSerializer
+
